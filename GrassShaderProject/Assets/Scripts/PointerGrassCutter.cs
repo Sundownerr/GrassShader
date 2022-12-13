f224ed33b7c97cd1cdbb851fbf076b1cb85e9528
egor.riazanov <ryazanov.e2@gmail.com>
2022-12-13 15:06:31 +0400

    add pointer grass cutter

---
+++ GrassShaderProject/Assets/Scripts/PointerGrassCutter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Grass
+{
+    public class PointerGrassCutter : GrassCutter
+    {
+        private Camera _camera;
+        [SerializeField] private GrassGPUInstancing[] _grass;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            foreach (var grassGPUInstancing in _grass)
+            {
+                grassGPUInstancing.AddGrassCutter(this);
+            }
+           
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButton(0))
+            {
+                return;
+            }
+            
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hitInfo, 1000))
+            {
+                transform.position = hitInfo.point;
+            }
+        }
+    }
+}
