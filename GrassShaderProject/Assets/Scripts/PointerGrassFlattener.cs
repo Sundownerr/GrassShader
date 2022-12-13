b0bb03e5618e8ae98060b2956fc36dc4fe63d2d1
egor.riazanov <ryazanov.e2@gmail.com>
2022-12-13 15:06:41 +0400

    add pointer grass flattener

---
+++ GrassShaderProject/Assets/Scripts/PointerGrassFlattener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Grass
+{
+    public class PointerGrassFlattener : GrassFlattener
+    {
+        private Camera _camera;
+        [SerializeField] private GrassGPUInstancing[] _grass;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+
+            foreach (var grassGPUInstancing in _grass)
+            {
+                grassGPUInstancing.AddGrassFlattener(this);
+            }
+
+        }
+
+        private void Update()
+        {
+            if (!Input.GetMouseButton(1))
+            {
+                return;
+            }
+
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hitInfo, 1000))
+            {
+                transform.position = hitInfo.point;
+            }
+        }
+    }
+}
